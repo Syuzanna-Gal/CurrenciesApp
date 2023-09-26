9651c6c815b8aed6b0f5f5b3a6cb34b862e17c5e
Syuzanna-Gal <syuzngal@gmail.com>
2023-09-26 17:47:52 +0400

    add colors and names

--- app/src/main/java/com/example/currenciesapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/currenciesapp/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//TODO change color names
+//main
+val Primary = Color(0xFF0A2FA7)
+val LightPrimary = Color(0xFFDEE4F8)
+val OnPrimary = Color(0xFFFFFFFF)
+val Secondary = Color(0xFF9DACDC)
+val TextDefault = Color(0xFF343138)
+val TextSecondary = Color(0xFF767676)
+val Outline = Color(0xFFEBEBEB)
+val Yellow = Color(0xFFFFB800)
+
+//bg
+val Header = Color(0xFFF6F6F6)
+val Card = Color(0xFFF0F2F8)
